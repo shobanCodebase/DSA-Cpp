d9637b187a6d05a7a313d5ca71b67fc059a709a2
Shoban <shoban.s2007@gmail.com>
2026-08-12 10:51:22 +0530

    min stack

---
+++ leetcode/0024_min_stack.cpp
@@ -0,0 +1,37 @@
+#include <stack>
+#include <algorithm>
+using namespace std;
+
+class MinStack {
+private:
+    stack<int> mainStack;
+    
+    stack<int> minStack;
+    
+public:
+    MinStack() {
+    }
+    
+    void push(int val) {
+        mainStack.push(val);
+        
+        if (minStack.empty()) {
+            minStack.push(val);
+        } else {
+            minStack.push(min(val, minStack.top()));
+        }
+    }
+    
+    void pop() {
+        mainStack.pop();
+        minStack.pop();
+    }
+    
+    int top() {
+        return mainStack.top();
+    }
+    
+    int getMin() {
+        return minStack.top();
+    }
+};
