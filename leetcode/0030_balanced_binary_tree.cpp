add6789eeb5fe5b638142f6c08d26a5b10fb5570
Shoban <shoban.s2007@gmail.com>
2026-08-15 13:45:23 +0530

    balanced binary tree

---
+++ leetcode/0030_balanced_binary_tree.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    bool isBalanced(TreeNode* root) {
+        return checkHeight(root) != -1;
+    }
+    
+private:
+    int checkHeight(TreeNode* node) {
+        if (node == nullptr) {
+            return 0;
+        }
+        
+        int leftHeight = checkHeight(node->left);
+        
+        if (leftHeight == -1) {
+            return -1;
+        }
+        
+        int rightHeight = checkHeight(node->right);
+        
+        if (rightHeight == -1) {
+            return -1;
+        }
+        
+        if (abs(leftHeight - rightHeight) > 1) {
+            return -1;
+        }
+        
+        return 1 + max(leftHeight, rightHeight);
+    }
+};
