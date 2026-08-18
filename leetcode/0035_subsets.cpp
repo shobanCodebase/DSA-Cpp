05fcdf04f233c3d8834fff7a1c93a48a203bed7c
Shoban <shoban.s2007@gmail.com>
2026-08-18 09:10:10 +0530

    subsets

---
+++ leetcode/0035_subsets.cpp
@@ -0,0 +1,31 @@
+class Solution {
+public:
+    vector<vector<int>> subsets(vector<int>& nums) {
+        vector<vector<int>> result;
+        vector<int> path;  // current partial subset being built
+        
+        backtrack(nums, 0, path, result);
+        
+        return result;
+    }
+    
+private:
+    void backtrack(vector<int>& nums, int start, vector<int>& path, 
+                    vector<vector<int>>& result) {
+        
+       
+        result.push_back(path);
+        
+        
+        for (int i = start; i < nums.size(); i++) {
+            
+            path.push_back(nums[i]);
+            
+           
+            backtrack(nums, i + 1, path, result);
+            
+           
+            path.pop_back();
+        }
+    }
+};
