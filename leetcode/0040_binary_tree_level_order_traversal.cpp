6e2de5f6462fe802e1d8666d24361e8c8705eaf4
Shoban <shoban.s2007@gmail.com>
2026-08-20 09:07:23 +0530

    binary tree level order traversal

---
+++ leetcode/0040_binary_tree_level_order_traversal.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+     vector<vector<int>> result;
+        
+        if (root == nullptr) {
+            return result;
+        }
+        
+        queue<TreeNode*> q;
+        q.push(root);
+        
+        while (!q.empty()) {
+            
+            
+            int levelSize = q.size();
+            
+            vector<int> currentLevel;
+            
+            
+            for (int i = 0; i < levelSize; i++) {
+                
+                TreeNode* node = q.front();
+                q.pop();
+                
+                currentLevel.push_back(node->val);
+                
+                
+                if (node->left != nullptr) {
+                    q.push(node->left);
+                }
+                if (node->right != nullptr) {
+                    q.push(node->right);
+                }
+            }
+            
+            result.push_back(currentLevel);
+        }
+        
+        return result;   
+    }
+};
