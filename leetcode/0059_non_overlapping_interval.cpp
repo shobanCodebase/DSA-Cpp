62613d17a20b8362841ee6eb45c59d37a71fb1f3
Shoban <shoban.s2007@gmail.com>
2026-08-30 14:06:41 +0530

    non overlapping interval

---
+++ leetcode/0059_non_overlapping_interval.cpp
@@ -0,0 +1,31 @@
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    int eraseOverlapIntervals(vector<vector<int>>& intervals) {
+        if (intervals.empty()) {
+            return 0;
+        }
+        
+        sort(intervals.begin(), intervals.end(), 
+             [](const vector<int>& a, const vector<int>& b) {
+                 return a[1] < b[1];
+             });
+        
+        int lastEnd = intervals[0][1];
+        int removalCount = 0;
+        
+        for (int i = 1; i < intervals.size(); i++) {
+            if (intervals[i][0] >= lastEnd) {
+                lastEnd = intervals[i][1];
+            } else {
+               
+                removalCount++;
+            }
+        }
+        
+        return removalCount;
+    }
+};
