31365275879e61c1778e8194703a5f91ede191fd
Shoban <shoban.s2007@gmail.com>
2026-09-01 06:48:49 +0530

    tax in chefland

---
+++ codechef/0002_tax_in_chefland.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+    cin>>t;
+    while(t--){
+    int a;
+    int b;
+    cin >> a;
+    if (a > 100){
+        cout<< a-10 << endl;
+    }
+    else {
+        cout<< a << endl;
+    }
+    }
+    return 0;
+}
