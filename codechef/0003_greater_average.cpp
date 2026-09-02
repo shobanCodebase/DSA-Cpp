7b57823b7f8c6c13ffd7578a164b7b237f9576cb
Shoban <shoban.s2007@gmail.com>
2026-09-02 05:52:40 +0530

    greater average

---
+++ codechef/0003_greater_average.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+    cin >> t;
+    while(t--){
+     int a, b, c;
+     cin >> a >> b >> c;
+     if(a+b > 2*c){
+         cout<<"YES"<<endl;
+     }
+     else{
+         cout<<"NO"<<endl;
+     }
+     
+    }
+    return 0;
+}
