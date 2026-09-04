e9f326a7b42056aaa516c97458da80c3b047d1b8
Shoban <shoban.s2007@gmail.com>
2026-09-04 09:54:59 +0530

    minimum pizzas

---
+++ codechef/0012_minimum_pizzas.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>> t;
+	while(t--){
+	int n, x;
+	cin >> n >> x;
+	cout << (((n*x)+3)/4) <<endl;
+
+	}
+    return 0;
+}
