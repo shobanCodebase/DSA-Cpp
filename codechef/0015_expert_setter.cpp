b7ee642fcc1e0288ec6e6acd7076c2ad5892a803
Shoban <shoban.s2007@gmail.com>
2026-09-05 05:52:13 +0530

    expert setter

---
+++ codechef/0015_expert_setter.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int x, y;
+	    cin >> x >> y;
+	    if(2 * y >= x){
+	        cout<< "YES"<< endl;
+	    }
+	    else {
+	        cout<< "NO"<<endl;
+	    }
+	}
+    return 0;
+}
