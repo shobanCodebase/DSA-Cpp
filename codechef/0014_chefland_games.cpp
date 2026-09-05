e7b1ddf955a9568e787f7e8a9892fa6c238c4140
Shoban <shoban.s2007@gmail.com>
2026-09-05 05:42:08 +0530

    chefland games

---
+++ codechef/0014_chefland_games.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin>> t;
+	while(t--){
+	    int a, b, c, d;
+	    cin >> a >> b >> c >>d;
+	    if (a + b + c + d < 1){
+	        cout<< "IN"<< endl;
+	    }
+	    else {
+	        cout << "OUT" << endl;
+	    }
+	}
+
+}
