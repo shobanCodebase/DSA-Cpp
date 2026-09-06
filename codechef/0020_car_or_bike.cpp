3e2257c702eda63bb081a0c889c6336ac8511145
Shoban <shoban.s2007@gmail.com>
2026-09-06 06:18:30 +0530

    car or bike

---
+++ codechef/0020_car_or_bike.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while (t--){
+	    int x, y;
+	    cin >> x >> y;
+	    if (x>y){
+	        cout << "CAR" << endl;
+	    }
+	    else if (x<y){
+	        cout << "BIKE" << endl;
+	    }
+	    else {
+	        cout << "SAME" << endl;
+	    }
+	}
+    return 0;
+}
