fb3dd941a330497cdece579a59f5a6b7c6b6760e
Shoban <shoban.s2007@gmail.com>
2026-09-06 05:42:51 +0530

    Sugarcane Juice Business

---
+++ codechef/0017_sugarcane_juice_business.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    long long total_profit = n * 15;
+	    cout << total_profit <<endl;
+	}
+    return 0;
+}
