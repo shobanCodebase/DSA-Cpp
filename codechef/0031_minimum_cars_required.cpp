6b66b7ffd90d3f27e7da0b47cc62a8900d3e8326
Shoban <shoban.s2007@gmail.com>
2026-09-12 05:17:43 +0530

    Minimum Cars required

---
+++ codechef/0031_minimum_cars_required.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    int cars = (n + 3) / 4;
+        cout << cars << endl;
+	}
+    return 0;
+}
