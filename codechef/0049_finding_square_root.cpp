056e73127976bed4c3f102b0c6aae5cd29d7e8f3
Shoban <shoban.s2007@gmail.com>
2026-09-21 05:43:56 +0530

    Finding Square Roots

---
+++ codechef/0049_finding_square_root.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	int t;
+	cin >> t;
+	while(t--){
+	    int n;
+	    cin >> n;
+	    int result = sqrt(n);
+	    cout << result << endl;
+	}
+    return 0;
+}
